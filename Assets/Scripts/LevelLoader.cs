00d228b4e885b76b4dbfcd562635acb2115a167c
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2020-06-17 15:09:05 +0100

    Added a splashscreen, some music and implemented a basic level loading script

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    int currentSceneIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        Invoke("loadNextLevel", 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void loadNextLevel()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
