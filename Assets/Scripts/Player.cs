adf7426635581fb42fe687636b727049b2dc55eb
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2020-06-19 18:41:47 +0100

    Added consistent music player to the splash screen Added rotation to the player ship to adjust pitch, roll & yaw to maintain consistent forward facing aim

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In meters per second (ms^-1")] [SerializeField] float xSpeed = 16;
+    [Tooltip("In meters per second (ms^-1")] [SerializeField] float ySpeed = 16;
+    [SerializeField] float xRange = 11f;
+    [SerializeField] float yRange = 7f;
+    [SerializeField] float positionPitchFactor = -2.77f;
+    [SerializeField] float controlPitchFactor = -15f;
+    [SerializeField] float positionYawFactor = 2.5f;
+    [SerializeField] float controlRollFactor = -30f;
+
+    float xThrow, yThrow;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+        float yaw = transform.localPosition.x * positionYawFactor;
+
+        float roll = xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = Input.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float xPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        yThrow = Input.GetAxis("Vertical");
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float yPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(xPos, yPos, transform.localPosition.z);
+    }
+}
