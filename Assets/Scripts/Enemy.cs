983ade8c15cda1a4334ff02baee61cdbaf58c810
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2020-06-20 21:24:15 +0100

    Added enemy DeathFX & Implemented Scoreboard

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,13 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    BoxCollider _boxCollider;
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!gameObject.GetComponent<BoxCollider>()) {
+            _boxCollider = gameObject.AddComponent<BoxCollider>();
+            _boxCollider.isTrigger = false;
+        }
     }
 
     // Update is called once per frame
@@ -19,6 +26,9 @@ public class Enemy : MonoBehaviour
     private void OnParticleCollision(GameObject other)
     {
         print("Particles collided with enemy" + gameObject.name);
+        GameObject explosion = Instantiate(deathFX, transform.position, Quaternion.identity);
+        explosion.GetComponent<ParticleSystem>().Play();
+        explosion.transform.parent = parent;
         Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/Scoreboard.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scoreboard : MonoBehaviour
+{
+    int score;
+    Text scoreText;
+    [SerializeField] int scorePerHit = 12;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        UpdateScoreText();
+    }
+
+    private void UpdateScoreText()
+    {
+        scoreText.text = score.ToString();
+    }
+
+    public void ScoreHit()
+    {
+        score = score + scorePerHit;
+        UpdateScoreText();
+    }
+}
